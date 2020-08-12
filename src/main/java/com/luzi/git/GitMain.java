7edf9e5760c7240b3cb8a8629b40bcf58e900535
luzi-smile <41979229+luzi-smile@users.noreply.github.com>
2020-08-12 09:58:51 +0800

    Update GitMain.java

--- src/main/java/com/luzi/git/GitMain.java
+++ src/main/java/com/luzi/git/GitMain.java
@@ -6,6 +6,7 @@ public class GitMain {
     public static void main(String[] args){
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 
 }
