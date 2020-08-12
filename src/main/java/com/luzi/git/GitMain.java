ecd256c7930bce91627b507679b7951d17e3156a
28106 胡振路 <zhenlu.hu@hand-china.com>
2020-08-12 09:53:45 +0800

    项目入口类 第二次提交

--- src/main/java/com/luzi/git/GitMain.java
+++ src/main/java/com/luzi/git/GitMain.java
@@ -4,7 +4,8 @@ package com.luzi.git;
 public class GitMain {
 
     public static void main(String[] args){
-        System.out.println("SSSS");
+        System.out.println("first commit");
+        System.out.println("second commit");
     }
 
 }
